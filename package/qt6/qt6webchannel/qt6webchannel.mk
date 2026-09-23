4b0c628382fc0bb2bfd943c23312bfe6d303a24f
Rhidian De Wit <rhidian.dewit@openpixelsystems.org>
2026-09-23 14:58:56 +0200

    Add QT6 WebEngine & WebChannel
    Signed-off-by: Rhidian De Wit <rhidian.dewit@openpixelsystems.org>

---
+++ package/qt6/qt6webchannel/qt6webchannel.mk
@@ -0,0 +1,42 @@
+################################################################################
+#
+# qt6webchannel
+#
+################################################################################
+
+QT6WEBCHANNEL_VERSION = $(QT6_VERSION)
+QT6WEBCHANNEL_SITE = $(QT6_SITE)
+QT6WEBCHANNEL_SOURCE = qtwebchannel-$(QT6_SOURCE_TARBALL_PREFIX)-$(QT6WEBCHANNEL_VERSION).tar.xz
+QT6WEBCHANNEL_INSTALL_STAGING = YES
+QT6WEBCHANNEL_SUPPORTS_IN_SOURCE_BUILD = NO
+QT6WEBCHANNEL_CMAKE_BACKEND = ninja
+QT6WEBCHANNEL_LICENSE = LGPL-3.0 or GPL-2.0 or GPL-3.0
+
+QT6WEBCHANNEL_LICENSE_FILES = \
+	LICENSES/BSD-3-Clause.txt \
+	LICENSES/GFDL-1.3-no-invariants-only.txt \
+	LICENSES/GPL-2.0-only.txt \
+	LICENSES/GPL-3.0-only.txt \
+	LICENSES/LGPL-3.0-only.txt
+
+QT6WEBCHANNEL_CONF_OPTS = \
+	-DBUILD_WITH_PCH=OFF \
+	-DQT_BUILD_EXAMPLES=OFF \
+	-DQT_BUILD_TESTS=OFF \
+	-DQT_HOST_PATH=$(HOST_DIR)
+
+QT6WEBCHANNEL_DEPENDENCIES = qt6base
+
+ifeq ($(BR2_PACKAGE_QT6DECLARATIVE),y)
+QT6WEBCHANNEL_DEPENDENCIES += qt6declarative
+endif
+
+ifeq ($(BR2_PACKAGE_QT6WEBCHANNEL_QUICK),y)
+QT6WEBCHANNEL_DEPENDENCIES += qt6declarative
+endif
+
+ifeq ($(BR2_PACKAGE_QT6WEBSOCKETS),y)
+QT6WEBCHANNEL_DEPENDENCIES += qt6websockets
+endif
+
+$(eval $(cmake-package))
